05ad0d6130f975af4893f2d5e1436d48e3c91e4b
pooruss <shihaoliang0828@163.com>
2023-10-07 16:15:30 +0800

    init

---
+++ FITE7410-FFA-Fall2023/assignment1/sample.R
@@ -0,0 +1,23 @@
+#This is a sample R script for assignment 1.
+
+# Install packages/load libraries
+install.packages('package_name')
+library(package_name)
+...
+
+
+# Load the data
+dataset = read.csv("file_path")
+
+
+#Exploratory Data Analysis, e.g., Distinguish Attributes, Univariate Analysis 
+str(dataset)
+summary(dataset)
+...
+
+ggplot(...)
+hist(...)
+...
+
+
+
